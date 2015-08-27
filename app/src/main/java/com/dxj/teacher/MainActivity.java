837eba41a6c03fae130b5e6c8d630d0d0f941669
zjb <1009784927@qq.com>
2015-08-27 11:43:52 +0800

    环信登录测试，修改老师端的友盟appkey和message secret

--- app/src/main/java/com/dxj/teacher/MainActivity.java
+++ app/src/main/java/com/dxj/teacher/MainActivity.java
@@ -15,6 +15,7 @@ import com.android.volley.VolleyError;
 import com.dxj.teacher.activity.ChatActivity;
 import com.dxj.teacher.activity.LoginAndRightActivity;
 import com.dxj.teacher.activity.UpdateImageActivity;
+import com.dxj.teacher.application.MyApplication;
 import com.dxj.teacher.base.BaseActivity;
 import com.dxj.teacher.factory.FragmentFactory;
 import com.dxj.teacher.http.CustomStringRequest;
@@ -22,15 +23,22 @@ import com.dxj.teacher.http.FinalData;
 import com.dxj.teacher.http.VolleySingleton;
 import com.dxj.teacher.utils.SPUtils;
 import com.easemob.EMCallBack;
+import com.easemob.applib.controller.HXSDKHelper;
 import com.easemob.chat.EMChatManager;
 import com.easemob.chat.EMGroupManager;
+import com.easemob.chatuidemo.Constant;
+import com.easemob.chatuidemo.DemoHXSDKHelper;
+import com.easemob.chatuidemo.db.UserDao;
+import com.easemob.chatuidemo.domain.User;
 import com.umeng.common.message.UmengMessageDeviceConfig;
 import com.umeng.message.IUmengRegisterCallback;
 import com.umeng.message.MsgConstant;
 import com.umeng.message.PushAgent;
 import com.umeng.message.UmengRegistrar;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends BaseActivity {
@@ -72,16 +80,44 @@ public class MainActivity extends BaseActivity {
         Log.i("TAG", "device_token " + device_token);
         SPUtils.saveSPData("token", device_token);
 
+        //        环信账号登录
         EMChatManager.getInstance().login("S1", "123456", new EMCallBack() {//回调
             @Override
             public void onSuccess() {
+
+                // 登陆成功，保存用户名密码
+                MyApplication.getInstance().setUserName("S1");
+                MyApplication.getInstance().setPassword("123456");
                 runOnUiThread(new Runnable() {
                     public void run() {
-                        EMGroupManager.getInstance().loadAllGroups();
-                        EMChatManager.getInstance().loadAllConversations();
-                        Log.d("main", "登陆聊天服务器成功！");
+                        try {
+                            // ** 第一次登录或者之前logout后再登录，加载所有本地群和回话
+                            // ** manually load all local groups and
+                            EMGroupManager.getInstance().loadAllGroups();
+                            EMChatManager.getInstance().loadAllConversations();
+                            Log.d("main", "登陆聊天服务器成功！");
+                            // 处理好友和群组
+                            initializeContacts();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            // 取好友或者群聊失败，不让进入主页面
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    DemoHXSDKHelper.getInstance().logout(true, null);
+                                    Toast.makeText(getApplicationContext(), R.string.login_failure_failed, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            return;
+                        }
+
                     }
                 });
+                // 更新当前用户的nickname 此方法的作用是在ios离线推送时能够显示用户nick
+                boolean updatenick = EMChatManager.getInstance().updateCurrentUserNick(
+                        MyApplication.currentUserNick.trim());
+                if (!updatenick) {
+                    Log.e("LoginActivity", "update current user nick fail");
+                }
             }
 
             @Override
@@ -98,6 +134,43 @@ public class MainActivity extends BaseActivity {
 //        register(device_token);
 
     }
+
+    //    环信账号登录后的处理
+    private void initializeContacts() {
+        Map<String, User> userlist = new HashMap<String, User>();
+        // 添加user"申请与通知"
+        User newFriends = new User();
+        newFriends.setUsername(Constant.NEW_FRIENDS_USERNAME);
+        String strChat = getResources().getString(
+                R.string.Application_and_notify);
+        newFriends.setNick(strChat);
+
+        userlist.put(Constant.NEW_FRIENDS_USERNAME, newFriends);
+        // 添加"群聊"
+        User groupUser = new User();
+        String strGroup = getResources().getString(R.string.group_chat);
+        groupUser.setUsername(Constant.GROUP_USERNAME);
+        groupUser.setNick(strGroup);
+        groupUser.setHeader("");
+        userlist.put(Constant.GROUP_USERNAME, groupUser);
+
+        // 添加"Robot"
+        User robotUser = new User();
+        String strRobot = getResources().getString(R.string.robot_chat);
+        robotUser.setUsername(Constant.CHAT_ROBOT);
+        robotUser.setNick(strRobot);
+        robotUser.setHeader("");
+        userlist.put(Constant.CHAT_ROBOT, robotUser);
+
+        // 存入内存
+        ((DemoHXSDKHelper) HXSDKHelper.getInstance()).setContactList(userlist);
+        // 存入db
+        UserDao dao = new UserDao(this);
+        List<User> users = new ArrayList<User>(userlist.values());
+        dao.saveContactList(users);
+    }
+
+
 
     private void register(String token) {
         String url = FinalData.URL_VALUE + "login";
